a74f36f6c60c8a19c455568f6c8c6a2a50c04c30
Koltyn Palmer <koltynchiaro@gmail.com>
2019-07-07 19:51:30 -0600

    server w fs and Schema

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require("express");
+var fs = require('fs');
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 var PORT = 3000;
 
@@ -20,8 +22,58 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+// img path
+var imgPath = '/path/to/some/img.png';
+
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/populatedb");
+
+// example schema
+var schema = new Schema({
+  img: { data: Buffer, contentType: String }
+});
+
+// our model
+var A = mongoose.model('A', schema);
+
+mongoose.connection.on('open', function () {
+console.error('mongo is open');
+
+// empty the collection
+A.remove(function (err) {
+  if (err) throw err;
+
+  console.error('removed old docs');
+
+  // store an img in binary in mongo
+  var a = new A;
+  a.img.data = fs.readFileSync(imgPath);
+  a.img.contentType = 'image/png';
+  a.save(function (err, a) {
+    if (err) throw err;
+
+    console.error('saved img to mongo');
+
+    // start a demo server
+    var server = express.createServer();
+    server.get('/', function (req, res, next) {
+      A.findById(a, function (err, doc) {
+        if (err) return next(err);
+        res.contentType(doc.img.contentType);
+        res.send(doc.img.data);
+      });
+    });
+
+    server.on('close', function () {
+      console.error('dropping db');
+      mongoose.connection.db.dropDatabase(function () {
+        console.error('closing db connection');
+        mongoose.connection.close();
+      });
+    });
+
+
+///////////////
 
 // When the server starts, create and save a new User document to the db
 // The "unique" rule in the User model's schema will prevent duplicate users from being added to the server
